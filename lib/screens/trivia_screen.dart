9d6bc0743729710e27a90eac6e64225cca88a983
Your Name <chamidilshan55@gmail.com>
2023-03-02 11:56:48 +0530

    added question details card

--- lib/screens/trivia_screen.dart
+++ lib/screens/trivia_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mad_hack_quiz/widgets/question_details.dart';
 
 class TraciaScreen extends StatefulWidget {
   const TraciaScreen({Key? key}) : super(key: key);
@@ -19,125 +20,11 @@ class _TraciaScreenState extends State<TraciaScreen> {
       backgroundColor: Colors.blue,
       body: ListView.builder(
           itemBuilder: (context, index){
-            return  Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                width: double.infinity,
-                height: 280.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text('The data or an instruction given to a computer system is '
-                          'particularly called:',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      height: 40.0,
-                      padding: EdgeInsets.only(left: 25.0, right: 25.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40.0),
-                      ),
-                      child: TextButton(
-                          onPressed: () {},
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                        ),
-                          child: Text('Input',
-                            style: TextStyle(
-                              color: Colors.white
-                            ),
-                          ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      height: 40.0,
-                      padding: EdgeInsets.only(left: 25.0, right: 25.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40.0),
-                      ),
-                      child: TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                        ),
-                        child: Text('Output',
-                          style: TextStyle(
-                              color: Colors.white
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      height: 40.0,
-                      width: double.infinity,
-                      padding: EdgeInsets.only(left: 25.0, right: 25.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100.0),
-                      ),
-                      child: TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                        ),
-                        child: Text('Entry',
-                          style: TextStyle(
-                              color: Colors.white
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      height: 40.0,
-                      padding: EdgeInsets.only(left: 25.0, right: 25.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40.0),
-                      ),
-                      child: TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                        ),
-                        child: Text('Information',
-                          style: TextStyle(
-                              color: Colors.white
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                  ],
-                ),
-              ),
-            );
+            return  QuestionDetail();
           },
         itemCount: 10,
           )
     );
   }
 }
+
---
+++ lib/widgets/question_details.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class QuestionDetail extends StatelessWidget {
+  const QuestionDetail({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        width: double.infinity,
+        height: 280.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.white
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text('The data or an instruction given to a computer system is '
+                  'particularly called:',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              height: 40.0,
+              padding: EdgeInsets.only(left: 25.0, right: 25.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: Text('Input',
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              height: 40.0,
+              padding: EdgeInsets.only(left: 25.0, right: 25.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: Text('Output',
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              height: 40.0,
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 25.0, right: 25.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100.0),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: Text('Entry',
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              height: 40.0,
+              padding: EdgeInsets.only(left: 25.0, right: 25.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: Text('Information',
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
